f003de5e60cf4e2fa3b3bfea749e5acee5d3bb86
Nandhanaa-Mathesvaran <nandhanaamathesvaran@gmail.com>
2025-10-16 09:42:08 +0530

    updated

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ age = st.number_input("Age", min_value=18, max_value=100, value=35)
 tenure = st.number_input("Tenure (years)", min_value=0, max_value=10, value=3)
 balance = st.number_input("Balance", min_value=0.0, value=50000.0)
 num_of_products = st.number_input("Number of Products", min_value=1, max_value=4, value=1)
-has_cr_card = st.checkbox("Has Credit Card")  
-is_active_member = st.checkbox("Is Active Member")  
+has_cr_card = st.number_input("Has Credit Card (0 or 1)", min_value=0, max_value=1, value=1)
+is_active_member = st.number_input("Is Active Member (0 or 1)", min_value=0, max_value=1, value=1)
 estimated_salary = st.number_input("Estimated Salary", min_value=0.0, value=50000.0)
 
 if st.button("Predict Churn"):
@@ -33,8 +33,8 @@ if st.button("Predict Churn"):
         "Tenure": tenure,
         "Balance": balance,
         "NumOfProducts": num_of_products,
-        "HasCrCard": int(has_cr_card),
-        "IsActiveMember": int(is_active_member),
+        "HasCrCard": has_cr_card,
+        "IsActiveMember": is_active_member,
         "EstimatedSalary": estimated_salary
     }
 
