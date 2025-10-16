db4112b8833e1f558dcada4b5f7aa0d58120750b
Nandhanaa-Mathesvaran <nandhanaamathesvaran@gmail.com>
2025-10-16 09:40:38 +0530

    updated

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import pickle
 
 st.set_page_config(page_title="Churn Prediction", layout="centered")
-
 st.title("Bank Customer Churn Prediction")
 
 with open("decision_tree_model.pkl", "rb") as file:
@@ -18,14 +17,13 @@ age = st.number_input("Age", min_value=18, max_value=100, value=35)
 tenure = st.number_input("Tenure (years)", min_value=0, max_value=10, value=3)
 balance = st.number_input("Balance", min_value=0.0, value=50000.0)
 num_of_products = st.number_input("Number of Products", min_value=1, max_value=4, value=1)
-has_cr_card = st.selectbox("Has Credit Card", ["Yes", "No"])
-is_active_member = st.selectbox("Is Active Member", ["Yes", "No"])
+has_cr_card = st.checkbox("Has Credit Card")  
+is_active_member = st.checkbox("Is Active Member")  
 estimated_salary = st.number_input("Estimated Salary", min_value=0.0, value=50000.0)
 
 if st.button("Predict Churn"):
     mapping_geo = {"France": 0, "Germany": 1, "Spain": 2}
     mapping_gender = {"Male": 1, "Female": 0}
-    mapping_yes_no = {"Yes": 1, "No": 0}
 
     input_dict = {
         "CreditScore": credit_score,
@@ -35,8 +33,8 @@ if st.button("Predict Churn"):
         "Tenure": tenure,
         "Balance": balance,
         "NumOfProducts": num_of_products,
-        "HasCrCard": mapping_yes_no[has_cr_card],
-        "IsActiveMember": mapping_yes_no[is_active_member],
+        "HasCrCard": int(has_cr_card),
+        "IsActiveMember": int(is_active_member),
         "EstimatedSalary": estimated_salary
     }
 
